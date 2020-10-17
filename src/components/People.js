d98e8d4b73ff2402c9e9eb30b70bbd7c9386bce4
xeusteerapat <xeus085@gmail.com>
2020-10-17 14:02:03 +0700

    [update] components

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import Person from './Person';
+
+const fetchPeople = async () => {
+  const config = {
+    method: 'get',
+    url: 'https://swapi.dev/api/people/',
+    headers: {},
+  };
+
+  const response = await axios(config);
+  const result = response.data;
+  return result;
+};
 
 const People = () => {
+  const { data, status } = useQuery('people', fetchPeople);
+
   return (
     <div>
       <h2>People</h2>
+      {status === 'loading' && <div>Loading</div>}
+      {status === 'error' && <div>Error fetching data</div>}
+      {status === 'success' && (
+        <div>
+          {data.results.map(person => (
+            <Person person={person} key={person.name} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };

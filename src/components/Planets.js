9026901147ac30d59a6017d8273e97c1036c47c5
xeusteerapat <xeus085@gmail.com>
2020-10-18 17:49:28 +0700

    [refactor] pagination with usePaginatedQuery

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useQuery } from 'react-query';
+import { usePaginatedQuery } from 'react-query';
 import axios from 'axios';
 import Planet from './Planet';
 
@@ -13,27 +13,47 @@ const fetchPlanets = async (key, page) => {
 
 const Planets = () => {
   const [page, setPage] = useState(1);
-  const { data, status } = useQuery(['planets', page], fetchPlanets, {
-    retry: 2,
-    staleTime: 0,
-    // cacheTime: 10,
-    onSuccess: () => console.log('data fetching successfully!'),
-  });
+  const { resolvedData, latestData, status } = usePaginatedQuery(
+    ['planets', page],
+    fetchPlanets,
+    {
+      retry: 2,
+      staleTime: 0,
+      // cacheTime: 10,
+      onSuccess: () => console.log('data fetching successfully!'),
+    }
+  );
 
   return (
     <div>
       <h2>Planets</h2>
-      <button onClick={() => setPage(1)}>Page 1</button>
-      <button onClick={() => setPage(2)}>Page 2</button>
-      <button onClick={() => setPage(3)}>Page 3</button>
       {status === 'loading' && <div>Loading</div>}
       {status === 'error' && <div>Error fetching data</div>}
       {status === 'success' && (
-        <div>
-          {data.results.map(planet => (
-            <Planet planet={planet} key={planet.name} />
-          ))}
-        </div>
+        <>
+          <button
+            onClick={() => setPage(prevVal => Math.max(prevVal - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous Page
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() =>
+              setPage(prevVal =>
+                !latestData || !latestData.next ? prevVal : prevVal + 1
+              )
+            }
+            disabled={!latestData || !latestData.next}
+          >
+            Next Page
+          </button>
+          <div>
+            {resolvedData.results.map(planet => (
+              <Planet planet={planet} key={planet.name} />
+            ))}
+          </div>
+        </>
       )}
     </div>
   );

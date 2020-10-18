5fc29236f813adcf025174ecb4823f09512ac95a
xeusteerapat <xeus085@gmail.com>
2020-10-18 17:31:55 +0700

    [update] pagination

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const response = await axios.get('https://swapi.dev/api/planets');
+const fetchPlanets = async (key, page) => {
+  const response = await axios.get(
+    `https://swapi.dev/api/planets/?page=${page}`
+  );
   const result = response.data;
   return result;
 };
 
 const Planets = () => {
-  const { data, status } = useQuery('planets', fetchPlanets, {
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(['planets', page], fetchPlanets, {
     retry: 2,
     staleTime: 0,
     // cacheTime: 10,
@@ -20,6 +23,9 @@ const Planets = () => {
   return (
     <div>
       <h2>Planets</h2>
+      <button onClick={() => setPage(1)}>Page 1</button>
+      <button onClick={() => setPage(2)}>Page 2</button>
+      <button onClick={() => setPage(3)}>Page 3</button>
       {status === 'loading' && <div>Loading</div>}
       {status === 'error' && <div>Error fetching data</div>}
       {status === 'success' && (
